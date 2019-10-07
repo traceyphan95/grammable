d5f38a0fb344a63446de1e2f4029a950e989190c
Tracey <traceyphan95@gmail.com>
2019-10-07 23:04:18 +0000

    14 failures

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,9 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
-  belongs_to :user
-  has_many :photos
+  validates :picture, presence: true
+  has_many :comments
+
   mount_uploader :picture, PictureUploader
+
+  belongs_to :user
 end
